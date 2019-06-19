5f60806e7e93a389580486e141ee3bf0cc748e67
Marcos Wrubel <mrcos.sou.eu@hotmail.com>
2019-06-19 09:11:10 -0300

    exercicio 3, atrasado mas entregue

--- app/src/main/java/com/example/meucafe/MainActivity.java
+++ app/src/main/java/com/example/meucafe/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.meucafe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +17,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.buttonFazerPedido);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent sendEmail = new Intent(Intent.ACTION_SEND);
+
+                // Tipo do conteúdo.
+                sendEmail.setType("text/plain");
+
+                // Para passar os destinatários deve-se utilizar um array com
+                // os endreços de e-mails.
+                String[] addresses = {"hewerton.oliveira@ifc.edu.br"};
+                sendEmail.putExtra(Intent.EXTRA_EMAIL, addresses);
+
+                // Corpo do e-mail.
+                sendEmail.putExtra(Intent.EXTRA_TEXT, "Olá, quero um café!");
+
+                // Assunto do e-mail.
+                sendEmail.putExtra(Intent.EXTRA_SUBJECT, "Café!");
+
+                // Verifica se existe algum app que resolve a intent
+                if (sendEmail.resolveActivity(getPackageManager()) != null) {
+
+                    // Inicia a intent
+                    startActivity(sendEmail);
+
+                    Log.i("E-mail", "Enviei o intent!");
+                }
+                Log.i("E-mail", "Botão pressionado!");
+
+            }
+        });
+
+
     }
 
     public void addCafe (View view) {
