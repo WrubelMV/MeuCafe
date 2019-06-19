2c1b2b1c766916f2430ed8ab2bbcedc2ec69dd23
Marcos Wrubel <mrcos.sou.eu@hotmail.com>
2019-06-19 08:50:53 -0300

    exercicio 3, atrasado mas entregue

--- app/src/main/java/com/example/meucafe/MainActivity.java
+++ app/src/main/java/com/example/meucafe/MainActivity.java
@@ -2,12 +2,59 @@ package com.example.meucafe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int quantidade = 0;
+    int valor = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addCafe (View view) {
+        valor = 2;
+        quantidade = 1;
+        TextView quant = findViewById(R.id.textViewCont);
+        quant.setText(quantidade++);
+        TextView valorTotal = findViewById(R.id.textViewValorTotal);
+        valorTotal.setText(valor++);
+
+    }
+
+    public void addCafeComLeite (View view) {
+        valor = 3;
+        quantidade = 1;
+        TextView quant = findViewById(R.id.textViewCont);
+        quant.setText(quantidade++);
+        TextView valorTotal = findViewById(R.id.textViewValorTotal);
+        valorTotal.setText(valor++);
+
+    }
+
+    public void addCapuccino (View view) {
+        valor = 3;
+        quantidade = 1;
+        TextView quant = findViewById(R.id.textViewCont);
+        quant.setText(quantidade++);
+        TextView valorTotal = findViewById(R.id.textViewValorTotal);
+        valorTotal.setText(valor++);
+
+    }
+
+    public String textViewPedido (int a, int b){
+        int preco = b*quantidade;
+
+        if (a == 1) {
+            return "O valor final será: R$" + preco + "pelo seu café";
+        }
+        else {
+            return "Seu pedido irá custar: R$" + preco + "pelos seus cafés desejados";
+        }
+
     }
 }
